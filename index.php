b53ffb1fc940cfb08e84f49f48053906b3d697e2
Sala D14 <CSalaD14@uniboyaca.edu.co>
2024-05-24 10:27:27 -0500

    PRIMERA ACTUALIZACION

---
+++ index.php
@@ -0,0 +1,33 @@
+<!-- iconos boostrap  -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+<link rel="stylesheet" href="./css/index.css">
+
+<html lang="es">
+    <main class="main-content">
+        <section class="login-box">
+            <section class="login-box-content">
+                <h2>Inicio De Sesion</h2>
+                    <form action="#">
+                        <!-- campos de sesion  -->
+                        <section>
+                            <input type="text" required>
+                            <label>Usuario</label>
+                            <i class="bi bi-person-circle"></i>
+                        </section>
+                        <section>
+                            <input type="password" required>
+                            <label>Contraseña</label>
+                            <i class="bi bi-lock-fill"></i>
+                        </section>
+                        <!-- boton  -->
+                        <button type="submit" class="btn-login">Iniciar Sesion</button>
+                    </form>
+            </section>
+            <section class="login-message">
+                <h2>Bienvenido A La Universidad De Boyaca</h2>
+            </section>
+        </section>
+        
+    </main>
+</html>
