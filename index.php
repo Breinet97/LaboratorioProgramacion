cb32a31fd263af2d9218a6c65893924a07a6e2c9
Breinet97 <babernal@uniboyaca.edu.co>
2024-05-24 11:57:27 -0500

    AAAAA

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
 <!-- iconos boostrap  -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-<link rel="stylesheet" href="./css/index.css">
+<link rel="stylesheet" href="./style.css">
 
 <html lang="es">
     <main class="main-content">
         <section class="login-box">
-            <section class="login-box-content">
-                <h2>Inicio De Sesion</h2>
-                    <form action="#">
+            <section class="login-box__content">
+                <h2 class="login-box__title">Inicio De Sesion</h2>
+                    <form class="login-box__form" action="#">
                         <!-- campos de sesion  -->
-                        <section>
-                            <input type="text" required>
-                            <label>Usuario</label>
-                            <i class="bi bi-person-circle"></i>
+                        <section class="login-box__form-input">
+                            <input class="login-box__input" type="text" required>
+                            <label class="login-box__label">Usuario</label>
+                            <i class="bi bi-person-circle login-box__icon"></i>
                         </section>
-                        <section>
-                            <input type="password" required>
-                            <label>Contraseña</label>
-                            <i class="bi bi-lock-fill"></i>
+                        <section class="login-box__form-input">
+                            <input class="login-box__input" type="password" required>
+                            <label class="login-box__label">Contraseña</label>
+                            <i class="bi bi-lock-fill login-box__icon"></i>
                         </section>
                         <!-- boton  -->
-                        <button type="submit" class="btn-login">Iniciar Sesion</button>
+                        <button type="submit" class="btn btn-login">Iniciar Sesion</button>
                     </form>
             </section>
-            <section class="login-message">
+            <section class="login-box__message">
                 <h2>Bienvenido A La Universidad De Boyaca</h2>
+                <p>Donde todos valemos vergaaa</p>
             </section>
         </section>
-        
     </main>
 </html>
