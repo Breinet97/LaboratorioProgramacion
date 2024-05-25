a0b47c9508407ad72cdb058ccb0ac68f9144159b
Breinet97 <babernal@uniboyaca.edu.co>
2024-05-25 10:45:58 -0500

    Front Terminado

--- index.php
+++ index.php
@@ -1,33 +1,44 @@
+
+
+
+
+
+
+
 <!-- iconos boostrap  -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-
-<link rel="stylesheet" href="./style.css">
+<!-- css -->
+<link rel="stylesheet" href="./css/style.css">
 
 <html lang="es">
-    <main class="main-content">
-        <section class="login-box">
-            <section class="login-box__content">
-                <h2 class="login-box__title">Inicio De Sesion</h2>
-                    <form class="login-box__form" action="#">
-                        <!-- campos de sesion  -->
-                        <section class="login-box__form-input">
-                            <input class="login-box__input" type="text" required>
-                            <label class="login-box__label">Usuario</label>
-                            <i class="bi bi-person-circle login-box__icon"></i>
-                        </section>
-                        <section class="login-box__form-input">
-                            <input class="login-box__input" type="password" required>
-                            <label class="login-box__label">Contraseña</label>
-                            <i class="bi bi-lock-fill login-box__icon"></i>
-                        </section>
-                        <!-- boton  -->
-                        <button type="submit" class="btn btn-login">Iniciar Sesion</button>
-                    </form>
-            </section>
-            <section class="login-box__message">
+<main class="main-content">
+    <section class="login-box">
+        <section class="login-box__content">
+            <form class="login-box__form" action="#">
+            <h2 class="login-box__title">Universidad De Boyaca</h2>
+                <!-- campos de sesion  -->
+                <section class="login-box__form-input">
+                    <input class="login-box__input" type="text" placeholder="Usuario" required>
+                    <i class="bi bi-person-circle login-box__icon"></i>
+                </section>
+                <section class="login-box__form-input">
+                    <input class="login-box__input" type="password" placeholder="Contraseña" required>
+                    <i class="bi bi-lock-fill login-box__icon"></i>
+                </section>
+                <!-- boton  -->
+                <button type="submit" class="btn-login">Iniciar Sesion
+                    <i class="bi bi-box-arrow-in-right login-box__icon login-box__icon--special"></i>
+                </button>
+            </form>
+        </section>
+        <section class="login-box__message">
+            <section class="login-box__message-section">
                 <h2>Bienvenido A La Universidad De Boyaca</h2>
                 <p>Donde todos valemos vergaaa</p>
             </section>
         </section>
-    </main>
+    </section>
+    <!-- ojito  -->
+</main>
+
 </html>
